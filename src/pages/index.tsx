7d851d41c64f1621586657b64138a7b46f313bbb
Tony <antonioaccess@gmail.com>
2022-12-20 19:41:53 -0800

    tmdb movie search results

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,10 +6,19 @@ import UserSummary from "../components/UserSummary";
 import MovieSummary from "../components/MovieSummary";
 import Search from "../components/common/Search";
 import { useState } from "react";
+import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
 	const [search, setSearch] = useState<string>("")
-	
+	const { data: resp } = trpc.movie.search.useQuery({ 
+    page: 1, 
+    searchTerm: search
+  }, {
+    onSuccess: (data) => {
+      console.log(data)
+    }
+  });
+  
 	return (
 		<>
 			<Head>
@@ -25,6 +34,16 @@ const Home: NextPage = () => {
 			</header>
 
 			<main className="w-full grid grid-cols-2 justify-items-center">
+        <ul>
+          {resp?.titles.map((title) => (
+            <li key={title.id}>
+              <figure>
+                <img src={title.poster_path} alt={title.title} />
+                <figcaption>{title.title}</figcaption>                
+              </figure>
+            </li>
+          ))}
+        </ul>
 				<EpisodeSummary />
 				<UserSummary />
 				<MovieSummary />
--- src/server/tmdb/client.ts
+++ src/server/tmdb/client.ts
@@ -2,23 +2,43 @@ import { env } from "../../env/server.mjs";
 
 const API_BASE_URL = "https://api.themoviedb.org/3",
 TRENDING_BASE_URL = `${API_BASE_URL}/trending/all/day?api_key=${env.TMDB_API_KEY}`,
-SEARCH_BASE_URL = `${API_BASE_URL}/search/movie?api_key=${env.TMDB_API_KEY}`,
+SEARCH_BASE_URL = `${API_BASE_URL}/search/movie?api_key=${env.TMDB_API_KEY}&language=en-US`,
 IMAGE_BASE_URL = "https://image.tmdb.org/t/p"
 
+interface Title {
+  id: number,
+  title: string,
+  backdrop_path: string,
+  poster_path: string,
+  overview: string,
+  release_date: string,
+  vote_average: number,
+  vote_count: number,
+  popularity: number,
+  media_type: string,
+}
+interface MovieSearchResponse {
+  page: number,
+  titles: Title[],
+}
 export const tmdb = {
-  getMovies: async(page: number, searchTerm="") => {
+  getMovies: async(page: number, searchTerm=""): Promise<MovieSearchResponse> => {
+
     const resp = await fetch(searchTerm
       ? `${SEARCH_BASE_URL}&query=${searchTerm}&page=${page}`
       : `${TRENDING_BASE_URL}&page=${page}`
     )
-    const titles = await resp.json()
-    titles.results = titles.results
-      .filter((title: any) => title.media_type === "movie")
-      .map((title: any) => ({
+    const res = await resp.json()
+    
+    const titles = res.results
+      .map((title: Title) => ({
         ...title,
 				backdrop_path: title.backdrop_path ? IMAGE_BASE_URL + "/w1280" + title.backdrop_path : null,
 				poster_path: title.poster_path ? IMAGE_BASE_URL + "/w342" + title.poster_path : null,
       }))
-    return titles
+    return {
+      page: res.page,
+      titles: titles,
+    }
   }
 }
