c16582bb5e2b341034e07831ed526a4ca4c7be7e
Tony <antonioaccess@gmail.com>
2022-12-25 06:12:29 -0800

    Movie select refetch

--- src/components/MovieSelect.tsx
+++ src/components/MovieSelect.tsx
@@ -19,7 +19,12 @@ const MovieSelect: FC<MovieSelectProps> = ({
   const { data: temp_title } = trpc.movie.getTitle.useQuery({ id: title?.id ?? 0 })
   const { data: movies, refetch: refreshMovies } = trpc.movie.getAll.useQuery()
   const { data: movie } = trpc.movie.get.useQuery({ id: selectedMovieKey })
-  const { mutate: addMovie } = trpc.movie.add.useMutation()
+  const { mutate: addMovie } = trpc.movie.add.useMutation({
+    onSuccess: (result) => {
+      setSelectedMovieKey(result.id)
+      refreshMovies()
+    }
+  })
   const handleChange = function(e: React.ChangeEvent<HTMLSelectElement>) {
     setSelectedMovieKey(e.target.value)
   }
@@ -35,7 +40,6 @@ const MovieSelect: FC<MovieSelectProps> = ({
       poster: title.poster_path,
       url: temp_title.imdb_path
     })
-    refreshMovies()
   }
   useEffect(() => {
     if (movie) {
@@ -44,21 +48,32 @@ const MovieSelect: FC<MovieSelectProps> = ({
     }
   }, [movie])
   return (    
-    <div className="w-full flex flex-col justify-center">
-      {selectedMovie && <MovieCard movie={selectedMovie} />}
+    <div className="w-full grid grid-cols-2">
+      <div className="w-full flex justify-center">
+        {selectedMovie && <MovieCard movie={selectedMovie} />}
+        {!selectedMovie && <div className="col-span-2">No movie selected</div>}
+      </div>
       <select className="text-gray-900 w-full rounded-md border-gray-300 shadow-sm focus:border-violet-300 focus:ring focus:ring-inset"
-        onChange={handleChange}        
+        onChange={handleChange}   
+        value={selectedMovieKey}     
       >
         <option value="00000000-0000-0000-0000-000000000000">Select a movie</option>
         {movies?.map((movie) => <option key={movie.id} value={movie.id}>{movie.title} <span className="text-xs">({movie.year})</span></option>)}
       </select>
+      <div className="w-full flex justify-center">
+        {!title && <div className="col-span-2">No title selected</div>}
+        {title && 
+          <>
+            <TitleCard title={title} />
+            <button className="bg-violet-500 hover:bg-violet-700 text-white font-bold py-2 px-4 rounded"
+              onClick={saveTitleAsMovie}
+            >
+              Save
+            </button>
+          </>
+        }
+      </div>
       <MovieSearch setTitle={setTitle} />
-      <button className="bg-violet-500 hover:bg-violet-700 text-white font-bold py-2 px-4 rounded"
-        onClick={saveTitleAsMovie}
-      >
-        {title && <TitleCard title={title} />}
-        Save
-      </button>
     </div>
 )}
 export default MovieSelect
