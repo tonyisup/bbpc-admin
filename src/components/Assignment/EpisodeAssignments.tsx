d2b14c1329374465fdd1d944d83db15fe2f92710
Tonyisup <antonioaccess@gmail.com>
2022-12-27 19:43:59 -0800

    episode assignments component

---
+++ src/components/Assignment/EpisodeAssignments.tsx
@@ -0,0 +1,47 @@
+
+import { Assignment, Episode } from "@prisma/client"
+import { FC, useState } from "react"
+import { HiChevronDown, HiChevronUp, HiX } from "react-icons/hi"
+import episode from "../../pages/episode"
+import { trpc } from "../../utils/trpc"
+import MovieCard from "../MovieCard"
+import AssignmentModal from "./AssignmentModal"
+
+interface EpisodeAssignmentsProps {
+	episode: Episode
+}
+
+const EpisodeAssignments: FC<EpisodeAssignmentsProps> = ({ episode }) => {
+	const [ showAssignments, setShowAssignments ] = useState<boolean>(false)
+  const { data: assignments, refetch: refreshAssignments } = trpc.assignment.getForEpisode.useQuery({ episodeId: episode.id})
+  const { mutate: removeAssignment } = trpc.assignment.remove.useMutation({
+    onSuccess: () => refreshAssignments(),
+  });
+  const handleRemoveAssignment = function(assignment: Assignment) {
+    removeAssignment({ id: assignment.id })
+  }
+	return (
+		<section className="flex flex-col w-full px-6">
+			<div className="flex justify-between w-full">
+				<h2 className="text-xl font-semibold">Assignments ({assignments?.length ?? 0})</h2>
+				{showAssignments && <button onClick={() => setShowAssignments(false)}><HiChevronUp /></button>}
+				{!showAssignments && <button onClick={() => setShowAssignments(true)}><HiChevronDown /></button>}
+				{episode && <AssignmentModal episode={episode} refreshItems={refreshAssignments} />}
+			</div>
+			{showAssignments && <div className="grid grid-cols-3 w-full">
+				{assignments?.map((assignment) => (
+					<>
+						{assignment.Movie && <MovieCard movie={assignment.Movie}/>}
+						{assignment.User && <span>{assignment.User.name}</span>}
+						<HiX 
+							className="text-red-500 cursor-pointer"
+							onClick={() => handleRemoveAssignment(assignment)}
+						/>
+					</>
+				))}
+			</div>}
+		</section>
+	)
+}
+
+export default EpisodeAssignments
--- src/pages/episode/[id].tsx
+++ src/pages/episode/[id].tsx
@@ -4,6 +4,7 @@ import Head from "next/head";
 import { useRouter } from "next/router";
 import { HiX } from "react-icons/hi";
 import AssignmentModal from "../../components/Assignment/AssignmentModal";
+import EpisodeAssignments from "../../components/Assignment/EpisodeAssignments";
 import MovieCard from "../../components/MovieCard";
 import { trpc } from "../../utils/trpc";
 
@@ -11,13 +12,6 @@ const Episode: NextPage = () => {
   const { query } = useRouter();
   const id = query.id as string;
   const { data: episode } = trpc.episode.get.useQuery({ id });
-  const { data: assignments, refetch: refreshAssignments } = trpc.assignment.getForEpisode.useQuery({ episodeId: id})
-  const { mutate: removeAssignment } = trpc.assignment.remove.useMutation({
-    onSuccess: () => refreshAssignments(),
-  });
-  const handleRemoveAssignment = function(assignment: Assignment) {
-    removeAssignment({ id: assignment.id })
-  }
   return (
     <>
       <Head>
@@ -30,24 +24,7 @@ const Episode: NextPage = () => {
           </h1>
         </header>
 
-        <section className="flex flex-col w-full px-6">
-          <div className="flex justify-between w-full">
-            <h2 className="text-xl font-semibold">Assignments ({assignments?.length ?? 0})</h2>
-            {episode && <AssignmentModal episode={episode} refreshItems={refreshAssignments} />}
-          </div>
-          <div className="grid grid-cols-3 w-full">
-            {assignments?.map((assignment) => (
-              <>
-                {assignment.Movie && <MovieCard movie={assignment.Movie}/>}
-                {assignment.User && <span>{assignment.User.name}</span>}
-                <HiX 
-                  className="text-red-500 cursor-pointer"
-                  onClick={() => handleRemoveAssignment(assignment)}
-                />
-              </>
-            ))}
-          </div>
-        </section>
+				{episode && <EpisodeAssignments episode={episode} />}
       </main>
     </>
   );
